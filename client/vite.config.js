20cd2dc92975817b4561cbae5ef33ff903a41f32
Jeyaruban Jenushan <jjenushan550@gmail.com>
2025-09-07 09:33:00 +0530

    The project successfully completed with https added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import fs from 'fs';
 import path from 'path';
 
+export default ({ mode }) => {
+  // Load .env file based on `mode`
+  const env = loadEnv(mode, process.cwd(), '');
 
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-   server: {
-    port: 5173,
-https: {
-  key: fs.readFileSync('localhost+2-key.pem'),
-  cert: fs.readFileSync('localhost+2.pem'),
-},
-    proxy: {
-      '/api': {
-     target: 'https://localhost:8443',
-        changeOrigin: true,
-        secure: false
-      }
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 5173,
+      https: {
+        key: fs.readFileSync(path.resolve(env.VITE_SSL_KEY_FILE)),
+        cert: fs.readFileSync(path.resolve(env.VITE_SSL_CRT_FILE)),
+      },
+      proxy: {
+        '/api': {
+          target: 'https://localhost:8443',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-      define: {
-    global: 'globalThis',
-  }
-  },
-})
-
+    define: {
+      global: 'globalThis',
+    },
+  });
+};
